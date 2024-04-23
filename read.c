358106fd94063a4ff730e84e69fd4630390d7423
Tyler Teufel <tteufel@localhost.localdomain>
2024-04-23 01:02:39 -0500

    FCFS works

--- read.c
+++ read.c
@@ -11,9 +11,15 @@ read_file(FILE *file_descriptor, task_t task_list[], int *size)
     /* read data from input file */
     /* Caveat: please place '&' before task_array[]; otherwise, your program will be 'core dumped' */
    int num = 0;
+   int i;
    while (fscanf(file_descriptor, "%u %u %u", &task_list[num].pid, &task_list[num].arrival_time, \
               &task_list[num].burst_time)!= EOF) {
         num++;
     }
     *size = num;
+    for (i = 0; i < num; i++) {
+        task_list[i].remaining_time = task_list[i].burst_time;
+        task_list[i].start_time = -1;
+        task_list[i].finish_time = -1;
+    }
 }
--- scheduler.c
+++ scheduler.c
@@ -34,6 +34,66 @@
 #include "open.h"
 #include "read.h"
 
+/*
+
+
+* wt[i] =  ( at[i – 1] + bt[i – 1] + wt[i – 1] ) – at[i]
+*
+*   where 
+*       at[i] = arrival time of current process
+*       wt[i] = waiting time of current process 
+*       at[i-1] = arrival time of previous process 
+*       bt[i-1] = burst time of previous process 
+*       wt[i-1] = waiting time of previous process 
+*       at[i] = arrival time of current process
+*
+* Average Waiting Time = (sum of all waiting time)/(Number of processes) 
+
+
+*/
+
+/* 
+ * First Come First Serve (FCFS) Policy
+ *
+ * Processes are scheduled and executed in the order that they arrive.
+ * Whichever process requests the CPU first is allocated the CPU first.
+*/
+float fcfs_policy(task_t task_array[], u_int count) {
+    int i = 0,time = 0, idle_t = 0;
+    //printf("FCFS Policy\n");
+    
+    while (i < count) {
+        
+        if(task_array[i].remaining_time == 0){
+            printf("<time %i> process %i is finished...\n", time, task_array[i].pid);
+            task_array[i].start_time = time; // Set start time to be the current time value on excecution.
+            i++;
+        }
+        else if(task_array[i].arrival_time <= time){
+            printf("<time %i> process %i is running\n", time, task_array[i].pid);
+            task_array[i].remaining_time--;
+            time++;
+        }
+        else{
+            printf("<time %i> idle...\n", time);
+            time++;
+            idle_t++;
+        }
+        
+
+    }
+
+    printf("<time %i> all processes are finished\n", time);
+
+    if(time == 0) {
+        return 0;
+    } else {
+        return (float)(time - idle_t)/time;
+    }
+
+    return 0.0;
+}
+
 int main( int argc, char *argv[] )  {
     char *file_name; /* file name from the commandline */
     FILE *fp; /* file descriptor */
@@ -49,7 +109,7 @@ int main( int argc, char *argv[] )  {
         return 0;
     }
 
-    if (argc != 2) {
+    if (argc < 2) {
         printf("Usage: input <file_name>\n");
         return EXIT_FAILURE;
     }
@@ -67,16 +127,27 @@ int main( int argc, char *argv[] )  {
     
     printf("Scheduling Policy: %s\n", argv[2]);
 
-    read_file(fp, &task_array, &count);
+    read_file(fp, task_array, &count);
 
     printf("There are %d tasks loaded from \"%s\". Press enter to continue...", count, argv[1]);
     getchar();
 
     printf("==================================================================\n");
 
-    // read_file(fp, task_array, &count);
-    // print_task_list(task_array, count);
+    if (strcmp(argv[2], "FCFS") == 0) {
+        cpu_usage = fcfs_policy(task_array, count);
+    } else if (strcmp(argv[2], "RR") == 0) {
+        // cpu_usage = rr_policy(task_array, count, atoi(argv[3]));
+        printf("RR Policy\n");
+    } else if (strcmp(argv[2], "SRFT") == 0) {
+        // cpu_usage = srft_policy(task_array, count);
+        printf("SRFT Policy\n");
+    } else {
+        printf("Invalid scheduling policy: %s\n", argv[2]);
+        return EXIT_FAILURE;
+    }
 
+   
     fclose(fp);
     return EXIT_SUCCESS;
 }
